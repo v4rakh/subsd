60daa0376c2d6f727814935a6c20d3e0aa3b4b49
Varakh <gituser@varakh.de>
2026-05-28 17:45:56 +0200

    misc: replace flake-utils and systems with flake-parts

--- flake.nix
+++ flake.nix
@@ -2,97 +2,100 @@
   description = "subsd flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    systems.url = "github:nix-systems/default";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    {
-      self,
+    inputs@{
+      flake-parts,
       nixpkgs,
-      flake-utils,
+      self,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        version = "0.3.1";
-        frontend = pkgs.stdenv.mkDerivation (finalAttrs: {
-          pname = "subsd-ui";
-          inherit version;
-          src = ./frontend;
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
-          nativeBuildInputs = with pkgs; [
-            nodejs_24
-            pnpm_10
-            pnpmConfigHook
-          ];
+      perSystem =
+        { pkgs, config, ... }:
+        let
+          version = "0.3.1";
+          frontend = pkgs.stdenv.mkDerivation (finalAttrs: {
+            pname = "subsd-ui";
+            inherit version;
+            src = ./frontend;
 
-          pnpmInstallFlags = [ "--frozen-lockfile" ];
+            nativeBuildInputs = with pkgs; [
+              nodejs_24
+              pnpm_10
+              pnpmConfigHook
+            ];
 
-          pnpmDeps = pkgs.fetchPnpmDeps {
-            inherit (finalAttrs)
-              pname
-              version
-              src
-              pnpmInstallFlags
-              ;
-            fetcherVersion = 3;
-            hash = "sha256-jwwpjIAmIoTmKbTGjY8BCqeA9PbA2UvJKywJxnYc8Sw=";
+            pnpmInstallFlags = [ "--frozen-lockfile" ];
+
+            pnpmDeps = pkgs.fetchPnpmDeps {
+              inherit (finalAttrs)
+                pname
+                version
+                src
+                pnpmInstallFlags
+                ;
+              fetcherVersion = 3;
+              hash = "sha256-jwwpjIAmIoTmKbTGjY8BCqeA9PbA2UvJKywJxnYc8Sw=";
+            };
+
+            buildPhase = ''
+              runHook preBuild
+              pnpm build
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out
+              cp -r ../web/dist $out/
+              runHook postInstall
+            '';
+          });
+        in
+        {
+          packages.frontend = frontend;
+
+          packages.server = pkgs.buildGoModule {
+            pname = "subsd";
+            inherit version;
+            src = ./.;
+            doCheck = false;
+            vendorHash = "sha256-caJMOPpPOfix+saH61Ka0o24FDq8QPkXWc/NdbA7CJA=";
+            ldflags = [
+              "-s"
+              "-w"
+            ];
+
+            preBuild = ''
+              mkdir -p web
+              cp -r ${frontend}/dist web/
+            '';
+            buildInputs = [ frontend ];
           };
 
-          buildPhase = ''
-            runHook preBuild
-            pnpm build
-            runHook postBuild
-          '';
-
-          installPhase = ''
-            runHook preInstall
-            mkdir -p $out
-            cp -r ../web/dist $out/
-            runHook postInstall
-          '';
-        });
-      in
-      {
-        packages.frontend = frontend;
-
-        packages.server = pkgs.buildGoModule {
-          pname = "subsd";
-          inherit version;
-          src = ./.;
-          doCheck = false;
-          vendorHash = "sha256-caJMOPpPOfix+saH61Ka0o24FDq8QPkXWc/NdbA7CJA=";
-          ldflags = [
-            "-s"
-            "-w"
-          ];
-
-          preBuild = ''
-            mkdir -p web
-            cp -r ${frontend}/dist web/
-          '';
-          buildInputs = [ frontend ];
+          packages.default = config.packages.server;
         };
 
-        packages.default = self.packages.${system}.server;
-      }
-    )
-    // {
-      nixosModules.default = import ./nix/module.nix {
-        inherit self;
-        lib = nixpkgs.lib;
-      };
+      flake = {
+        nixosModules.default = import ./nix/module.nix {
+          inherit self;
+          lib = nixpkgs.lib;
+        };
 
-      homeManagerModules.default = import ./nix/hm-module.nix {
-        inherit self;
-        lib = nixpkgs.lib;
+        homeManagerModules.default = import ./nix/hm-module.nix {
+          inherit self;
+          lib = nixpkgs.lib;
+        };
       };
     };
 }
